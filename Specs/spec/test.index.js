ac934160718d4544f8919b9042cc5a2921b0a10b
Drew Cuthbertson <syeoryn@gmail.com>
2014-10-02 22:03:10 -0700

    Creates container constructor

--- Specs/spec/test.index.js
+++ Specs/spec/test.index.js
@@ -1,6 +1,16 @@
-describe('Test 1', function(){
-  it('should pass', function(){
-    expect(true).toBe(true);
+describe('the container', function(){
+
+  var container;
+  beforeEach(function(){
+    container = new Container(4,4);
+  });
+
+  it('should contain a height method', function(){
+    expect(typeof container.getHeight).toBe("function");
+  });
+
+  it('should contain a width method', function(){
+    expect(typeof container.getWidth).toBe("function");
   });
 });
 
---
+++ index.js
@@ -0,0 +1,12 @@
+var Container = function(width, height){
+  this.width = width;
+  this.height = height;
+}
+
+Container.prototype.getWidth = function(){
+  return this.width;
+}
+
+Container.prototype.getHeight = function(){
+  return this.height;
+}
