3bdff72ac042351f411b2d658ae3edd045b043c6
Drew Cuthbertson <syeoryn@gmail.com>
2014-10-05 16:45:16 -0700

    (spec) Adds container dimension tests

--- Specs/spec/containerModel.test.js
+++ Specs/spec/containerModel.test.js
@@ -5,12 +5,12 @@ describe('the container', function(){
     container = new Container(4,4);
   });
 
-  it('should contain a height method', function(){
-    expect(typeof container.getHeight).toBe("function");
-  });
-
   it('should contain a width method', function(){
     expect(typeof container.getWidth).toBe("function");
+  });
+
+  it('should contain a height method', function(){
+    expect(typeof container.getHeight).toBe("function");
   });
 
   it('should contain an area method', function(){
@@ -18,3 +18,21 @@ describe('the container', function(){
   });
 });
 
+describe('container dimension methods', function(){
+  var container;
+  beforeEach(function(){
+    container = new Container(4,6);
+  });
+
+  it('should get the correct width', function(){
+    expect(container.getWidth()).toEqual(4);
+  });
+
+  it('should get the correct height', function(){
+    expect(container.getHeight()).toEqual(6);
+  });
+
+  it('should get the correct area', function(){
+    expect(container.getArea()).toEqual(24);
+  });
+});
