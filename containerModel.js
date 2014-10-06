ae402a5635e14e2aa99cbd8f1fcd86a958f1ccdf
Ryn830 <Ryan@Ryans-MacBook-Pro.local>
2014-10-05 19:14:28 -0700

    (docs) Removes comments from Container model

--- containerModel.js
+++ containerModel.js
@@ -1,12 +1,6 @@
 var Container = function(width, height){
   this.width = width;
   this.height = height;
-  // box locations
-  // # of empty spaces
-  // # of filled spaces
-  // split method
-  // add/ remove box
-  // conflict helpers
 }
 
 Container.prototype.getWidth = function(){
